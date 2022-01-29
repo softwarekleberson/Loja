2ec6828d669545e55dbe6af87dce507ffc85e9aa
softwarekleberson <santossilvakleberson@gmail.com>
2022-01-29 00:52:15 -0300

    parte 1

---
+++ src/main/java/br/com/alura/mvc/siteLoja/SiteLojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.mvc.siteLoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/mvc/siteLoja/SiteLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.mvc.siteLoja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
