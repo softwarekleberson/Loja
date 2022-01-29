3421fa15b297d98f36ce7b7402e46f6472a945b1
softwarekleberson <santossilvakleberson@gmail.com>
2022-01-29 16:03:10 -0300

    criacao do tenplate home e integracao com o spring data

---
+++ src/main/java/br/com/alura/mvc/siteLoja/repository/PedidoRepository.java
@@ -0,0 +1,12 @@
+package br.com.alura.mvc.siteLoja.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.alura.mvc.siteLoja.model.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+	
+
+}
+
